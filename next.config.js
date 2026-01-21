1fa6fe11d8725fc4f5ff3fa084caed620ad4ccb7
NOTTIBOY137 <nareshkandula54@gmail.com>
2026-01-21 07:05:56 -0500

    Fix: Exclude Supabase Edge Functions from Next.js build (Deno code)

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  webpack: (config, { isServer }) => {
+    // Exclude Supabase Edge Functions from webpack compilation
+    config.module.rules.push({
+      test: /supabase\/functions\/.*\.ts$/,
+      use: 'ignore-loader',
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
