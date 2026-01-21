808b5f27640c70a3b15a46a04b27a1ce6801e03e
NOTTIBOY137 <nareshkandula54@gmail.com>
2026-01-21 07:06:29 -0500

    Revert: Remove webpack config, tsconfig exclude is sufficient

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
   images: {
     domains: [],
   },
-  webpack: (config, { isServer }) => {
-    // Exclude Supabase Edge Functions from webpack compilation
-    config.module.rules.push({
-      test: /supabase\/functions\/.*\.ts$/,
-      use: 'ignore-loader',
-    })
-    return config
-  },
 }
 
 module.exports = nextConfig
